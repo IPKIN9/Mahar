bf70f213a1e12d6777d04ec8e00cd596c3b23275
IPKIN9 <irwandirini@gmail.con>
2021-09-26 14:52:47 +0800

    Change link logo

--- resources/views/Base/SideBar.blade.php
+++ resources/views/Base/SideBar.blade.php
@@ -3,14 +3,14 @@
         <li class="sidebar-title">Menu</li>
 
         <li class="sidebar-item {{ Route::is('dashboard.index') ? 'active' : '' }} ">
-            <a href="index.html" class='sidebar-link'>
+            <a href="{{route('dashboard.index')}}" class='sidebar-link'>
                 <i class="bi bi-grid-fill"></i>
                 <span>Dashboard</span>
             </a>
         </li>
         <li class="sidebar-item {{ Route::is('detail.index') ? 'active' : '' }}">
             <a href="{{route('detail.index')}}" class='sidebar-link'>
-                <i class="fas fa-ad"></i>
+                <i class="fas fa-info-circle"></i>
                 <span>Detail</span>
             </a>
         </li>
